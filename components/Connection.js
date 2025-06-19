61d1c309350c6b04fd016abee53de5158598ea91
Dmitriy <mrwantid@gmail.com>
2025-06-19 12:47:04 +0300

    правки в заголовках подключения

--- components/Connection.js
+++ components/Connection.js
@@ -103,7 +103,7 @@ const Connection = (props) => {
       let response = await fetch(`${config.hostname}/api/user/login/`, {
         method: 'POST',
         headers: {
-          'Accept': '*/*',
+          'Accept': 'application/json',
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
