0556c29e6da93575b06aa2c1239467604315aeeb
Dmitriy Kurilov <mrwantid@gmail.com>
2025-09-15 16:47:19 +0300

    Button: портрет

--- components/Button.js
+++ components/Button.js
@@ -1,24 +1,24 @@
 import { StyleSheet, Dimensions, TouchableOpacity, Text } from 'react-native';
 import { useTheme } from './ThemeContext';
+import { useResponsiveSizes } from './hooks/useResponsiveSizes';
 
 const Button = ({ title, disabled = false, onPress, style }) => {
     const { theme, toggleTheme } = useTheme();
-    const { height: screenHeight } = Dimensions.get('window');
-    const textSize = screenHeight * .0225;
-    const borderRadius = screenHeight * .02;
+    const sizes = useResponsiveSizes();
+    const borderRadius = sizes.windowHeight * .02;
 
     return (
         <TouchableOpacity
             style={[styles.button, {
-                padding: textSize * .8,
+                padding: sizes.textSize * .8,
                 borderRadius: borderRadius,
-                marginVertical: textSize * .5,
+                marginVertical: sizes.textSize * .5,
                 backgroundColor: disabled ? theme.container : theme.free,
             }, style]}
             onPress={!disabled ? onPress : null}
         >
             <Text style={[styles.buttonText, {
-                fontSize: textSize, opacity: disabled ? .3 : 1,
+                fontSize: sizes.textSize, opacity: disabled ? .3 : 1,
                 color: disabled ? theme.light : theme.dark
             }]}>{title}</Text>
         </TouchableOpacity>
